e5e548669e92eb7977e07013b240ff1f60c61e34
ipcoo43 <ipcoo43@gmail.com>
2021-01-27 15:11:01 +0900

    2021-01-27 lesson

---
+++ git.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#DATE=`date +%Y-%m-%d" "%H:%M`
+DATE=`date +%Y-%m-%d`
+MSG="$DATE lesson"
+if [ $# -gt 0 ]; then
+    MSG="$1"
+fi
+
+#git checkout master
+# git diff --numstat origin/master
+
+git add --all
+git commit -am "${MSG}"
+git push
+# git push origin master
---
+++ test/test.js
@@ -0,0 +1,2 @@
+console.log('숫자 입니다. %d', 10)
+console.log('문자열 입니다. %s', '안녕하세요')
